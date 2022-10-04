7562c3bbbe7f7a5d992280e75becc5fb157c99f3
Rahul Mehta <rmehta197@gmail.com>
2022-10-04 14:16:32 -0500

    added comments

--- generate_snp_per_chromosome.sh
+++ generate_snp_per_chromosome.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
+#Script to generate the SNPS from the reference chromosome of Human Genome European population
+#Each chromosome has it's own text file for each SNP
 
 for chr in {1..22}; do 
 
